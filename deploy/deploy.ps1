c907dc7dc6b0a21069b0e965d8b6ac7b26479374
John Downs <john.downs@microsoft.com>
2020-01-29 17:01:23 +1300

    Remove service principal from deployment script

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,6 +1,4 @@
 param(
-    $ServicePrincipalApplicationId,
-    $ServicePrincipalClientSecret,
     $ResourceGroupName,
     $ResourceGroupLocation,
     $AnalysisServicesDatabaseName
@@ -9,13 +7,9 @@ param(
 Write-Host "Creating resource group $ResourceGroupName in location $ResourceGroupLocation."
 az group create -n $ResourceGroupName -l $ResourceGroupLocation
 
-Write-Host "Looking up service principal object ID for application ID $ServicePrincipalApplicationId."
-$servicePrincipalObjectId = az ad sp list --filter "appId eq '$ServicePrincipalApplicationId'" --query '[].[objectId]' -o tsv
-Write-Host "Found service principal object ID $servicePrincipalObjectId."
-
 Write-Host 'Starting deployment of ARM template.'
 $templateFilePath = Join-Path $PSScriptRoot 'template.json'
-$deploymentOutputsJson = az group deployment create -j -g $ResourceGroupName --template-file $templateFilePath --parameters servicePrincipalApplicationId=$ServicePrincipalApplicationId servicePrincipalClientSecret=$ServicePrincipalClientSecret servicePrincipalObjectId=$servicePrincipalObjectId analysisServicesDatabaseName=$AnalysisServicesDatabaseName
+$deploymentOutputsJson = az group deployment create -j -g $ResourceGroupName --template-file $templateFilePath --parameters analysisServicesDatabaseName=$AnalysisServicesDatabaseName
 $deploymentOutputs = $deploymentOutputsJson | ConvertFrom-Json
 $functionAppName = $deploymentOutputs.properties.outputs.functionsAppName.value
 
